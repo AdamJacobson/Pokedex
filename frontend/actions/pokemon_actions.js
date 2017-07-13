efd39be119ee03b0a5c8157959b8e16a0a864d4d
AdamJacobson <amartenj@gmail.com>
2017-07-12 17:07:32 -0700

    Working getAllPokemon action

---
+++ frontend/actions/pokemon_actions.js
@@ -0,0 +1,12 @@
+export const RECEIVE_ALL_POKEMON = "RECEIVE_ALL_POKEMON";
+
+import * as APIUtil from '../util/api_util';
+
+export const receiveAllPokemon = (pokemon) => {
+  return {
+    type: RECEIVE_ALL_POKEMON,
+    pokemon
+  };
+};
+
+// APIUtil.fetchAllPokemon().then((data) => dispatch(receiveAllPokemon(data)));
--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { receiveAllPokemon } from "./actions/pokemon_actions";
+import * as APIUtil from './util/api_util';
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
   const rootEl = document.getElementById('root');
+  const store = configureStore();
+
+  window.dispatch = store.dispatch;
+  window.getState = store.getState;
+
+  window.receiveAllPokemon = receiveAllPokemon;
+  window.fetchAllPokemon = APIUtil.fetchAllPokemon;
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
